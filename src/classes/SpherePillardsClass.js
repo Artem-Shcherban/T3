11ce9a809952ad5d3436613dd6bd6bc982d27ee7
Your Name <you@example.com>
2022-07-20 13:09:11 +0500

    change music

--- src/classes/SpherePillardsClass.js
+++ src/classes/SpherePillardsClass.js
@@ -131,7 +131,7 @@ class SpherePillardsClass {
         if(SoundReactor.playFlag){
             let i = 0
        while(i < this.pillards.children.length){
-        this.pillards.children[i].children[0].position.y = SoundReactor.fdata[i] / 255 * 4  
+        this.pillards.children[i].children[0].position.y = SoundReactor.fdata[i] / 255 * 3  
         i++
     }
            
